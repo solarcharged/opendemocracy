ae971051cd46d6e207b913071363fccea0b3173b
solarcharged <info@solarcharged.net>
2016-10-20 09:15:33 +0700

    Create opendemocracy.py

---
+++ opendemocracy.py
@@ -0,0 +1,51 @@
+
+
+#opendemocracy.
+
+
+class scrutiny (object):
+
+	def check(self):
+		pass
+
+
+
+class legislation (object):
+
+	def check(self):
+		pass
+
+
+class decision (object):
+
+	def check(self):
+		pass
+
+
+
+
+
+class budget_taxes (object):
+
+
+	def check(self):
+		pass
+
+
+
+class debating (decision):
+
+	def check(self):
+		pass
+
+
+class new_parliment(object):
+
+	def __init__(self, open_decision):
+		pass
+
+	def vote(self, your_vote):
+		pass
+
+
+a_decision = new_parliment('decision')
